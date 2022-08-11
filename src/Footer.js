b2f456021eba6ac2cfb11edece5567a358513b1b
Akash R Prabhu <akashprabhu41@gmail.com>
2022-08-11 13:28:25 +0530

    footer ui completed with styling

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,48 @@
 import React from "react";
 import "./Footer.css";
+import { Grid, Slider } from "@mui/material";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
 function Footer() {
-  return <div className="footer">I am footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer_left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/3/3e/Calvin_Harris_and_Dua_Lipa_One_Kiss.png"
+          alt="song preview"
+        />
+        <div className="footer_songInfo">
+          <h4>One Kiss(with Dua Lipa)</h4>
+          <p>Calvin Harris,Dua Lipa</p>
+        </div>
+      </div>
+      <div className="footer_center">
+        <ShuffleIcon className="footer_green" />
+        <SkipPreviousIcon className="footer_icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer_icon" />
+        <SkipNextIcon className="footer_icon" />
+        <RepeatIcon className="footer_green" />
+      </div>
+      <div className="footer_right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
